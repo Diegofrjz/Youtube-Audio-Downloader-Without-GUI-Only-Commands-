971f84d8ce78218e0ceeb79a81bcf1b97f2fed3e
Diegofr <120336092+Diegofrjz@users.noreply.github.com>
2023-03-26 13:21:59 -0500

    Add files via upload

---
+++ MP3DowloaderWithoutUI.py
@@ -0,0 +1,26 @@
+import os
+from pytube import YouTube
+
+# Pedimos al usuario que ingrese la URL del video de YouTube
+video_url = input('Ingresa el enlace del video de YouTube: ')
+
+# Pedimos al usuario que ingrese la ubicación donde quiere guardar el archivo de audio
+audio_path = input('Ingresa la ubicación donde quieres guardar el archivo de audio (incluyendo el nombre de archivo): ')
+
+# Descargamos el video y extraemos el audio en formato mp3
+yt = YouTube(video_url)
+audio = yt.streams \
+    .filter(only_audio=True, file_extension='mp4') \
+    .order_by('abr') \
+    .desc() \
+    .first()
+
+if audio:
+    output_file = audio.download(output_path='/tmp')
+    base, ext = os.path.splitext(output_file)
+    new_file = base + '.mp3'
+    os.rename(output_file, new_file)
+    os.replace(new_file, audio_path)
+    print('Audio descargado en', audio_path)
+else:
+    print('No se encontró ningún audio en formato mp3 en el video.')
